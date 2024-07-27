30017cb415c890f5936b9e6017aa2d8cd5afb10f
Dustin Kelley <Kelleydustin2018@gmail.com>
2024-07-26 20:17:02 -0500

    wip: auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,27 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import 'react-native-reanimated';
-import { useColorScheme } from 'react-native'
-import { TamaguiProvider } from 'tamagui'
-import  config  from '../tamagui.config';
-import { useState, useEffect } from 'react'
-import { supabase } from '../lib/supabase'
-import Auth from '../components/Auth'
-import Account from '../components/Account'
-import { View } from 'react-native'
-import { Session } from '@supabase/supabase-js'
-
+import { useColorScheme } from 'react-native';
+import { TamaguiProvider } from 'tamagui';
+import config from '../tamagui.config';
+import { useState, useEffect } from 'react';
+import { supabase } from '../lib/supabase';
+import Auth from '../components/Auth';
+import Account from '../components/Account';
+import { View } from 'react-native';
+import { Session } from '@supabase/supabase-js';
+import AuthProvider from '@/providers/AuthProvider';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [session, setSession] = useState<Session | null>(null)
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -34,26 +37,18 @@ export default function RootLayout() {
     return null;
   }
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
 
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
 
   return (
-    
     <TamaguiProvider config={config} defaultTheme={colorScheme!}>
+      <AuthProvider>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        {session ?  <Stack>
+        <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-        </Stack> : <Auth />}
-       
+        </Stack>
       </ThemeProvider>
+      </AuthProvider>
     </TamaguiProvider>
   );
 }
