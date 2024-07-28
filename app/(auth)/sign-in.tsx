a040ecbce54fa92f93a826a3f03d59a1774a59b9
Dustin Kelley <Kelleydustin2018@gmail.com>
2024-07-28 17:00:57 -0500

    feat: add sign in page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
-import { Alert, StyleSheet, View, AppState, Button } from 'react-native'
+import { Alert, StyleSheet, TextInput } from 'react-native'
 import { supabase } from '@/lib/supabase'
-import { Input } from 'tamagui'
+import { Input, YStack, Text } from 'tamagui'
 import { Link, router } from 'expo-router'
+import { Button } from '@/design-components/components/Button'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 
 
 export default function Auth() {
+  const insets = useSafeAreaInsets();
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [loading, setLoading] = useState(false)
@@ -38,31 +41,51 @@ export default function Auth() {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Input
-          onChangeText={(text) => setEmail(text)}
-          value={email}
-          placeholder="email@address.com"
-          autoCapitalize={'none'}
-        />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Input
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-          secureTextEntry={true}
-          placeholder="Password"
-          autoCapitalize={'none'}
-        />
-      </View>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Button title="Sign in" disabled={loading} onPress={() => {signInWithEmail();   router.replace('/');}} />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Link href={'/sign-up'}  disabled={loading}>Sign up</Link>
-      </View>
-    </View>
+    <YStack
+      gap="$md"
+      paddingTop={insets.top}
+      flexGrow={1}
+      paddingHorizontal="$md"
+      justifyContent="space-between"
+    >
+      <YStack gap="$lg">
+        <YStack gap="$xs">
+          <Text fontSize={'$h1'} fontWeight={'$600'} color={'$purple2'}>
+            Sign In
+          </Text>
+          <Text>Please sign in to continue using the app</Text>
+        </YStack>
+
+        <YStack>
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            value={email}
+            onChangeText={setEmail}
+            placeholder="jon@gmail.com"
+            style={styles.input}
+          />
+
+          <Text style={styles.label}>Password</Text>
+          <TextInput
+            value={password}
+            onChangeText={setPassword}
+            placeholder=""
+            style={styles.input}
+            secureTextEntry
+          />
+        </YStack>
+      </YStack>
+      <YStack paddingBottom={insets.bottom + 10} gap="$xs" alignItems="center">
+        <Button variant="primary" onPress={signUpWithEmail} disabled={loading}>
+          <Button.Text fontSize={'$h4'}>
+            {loading ? 'Signing in...' : 'Sign In'}
+          </Button.Text>
+        </Button>
+        <Link href="/sign-up">
+          Already have an account? <Text color={'$purple2'}>Sign Up</Text>
+        </Link>
+      </YStack>
+    </YStack>
   )
 }
 
@@ -71,10 +94,22 @@ const styles = StyleSheet.create({
     marginTop: 40,
     padding: 12,
   },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginTop: 5,
+    marginBottom: 20,
+    backgroundColor: 'white',
+    borderRadius: 5,
+  },
   verticallySpaced: {
     paddingTop: 4,
     paddingBottom: 4,
     alignSelf: 'stretch',
+  },
+  label: {
+    color: 'gray',
   },
   mt20: {
     marginTop: 20,
