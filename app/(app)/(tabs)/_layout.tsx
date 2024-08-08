9674f2b7e21626b6571a7f9cdeb0733bf16b3e37
Dustin Kelley <Kelleydustin2018@gmail.com>
2024-08-07 21:04:24 -0500

    style: tabs

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -3,24 +3,32 @@ import React from 'react';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { getTokens } from 'tamagui';
 
-
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: getTokens().color.$purple1.val,
         headerShown: false,
-      }}>
+        tabBarStyle: {
+         // backgroundColor: getTokens().color.$gray3.val,
+          //borderTopWidth: 1,
+          height: 100,
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Budget',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <TabBarIcon
+              name={focused ? 'home' : 'home-outline'}
+              color={color}
+            />
           ),
         }}
       />
-        
+
       <Tabs.Screen
         name="transactions"
         options={{
@@ -30,12 +38,15 @@ export default function TabLayout() {
           ),
         }}
       />
-       <Tabs.Screen
+      <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'person' : 'person-outline'} color={color} />
+            <TabBarIcon
+              name={focused ? 'person' : 'person-outline'}
+              color={color}
+            />
           ),
         }}
       />
