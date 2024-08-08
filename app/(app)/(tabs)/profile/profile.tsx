cd6eb668819a9c029079f8fd296cae8734fe8866
Dustin Kelley <Kelleydustin2018@gmail.com>
2024-08-07 20:53:21 -0500

    feat: profile page UI

--- app/(app)/(tabs)/profile/profile.tsx
+++ app/(app)/(tabs)/profile/profile.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@/design-components/components/Button';
 import { supabase } from '@/lib/supabase';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Link } from 'expo-router';
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { YStack, Text } from 'tamagui';
+import { YStack, Text, XStack, getTokens } from 'tamagui';
 
 const Profile = () => {
   const insets = useSafeAreaInsets();
@@ -17,21 +18,94 @@ const Profile = () => {
       paddingHorizontal="$md"
       justifyContent="space-between"
     >
-      <YStack>
-        <Text fontWeight={'$600'} fontSize={'$h2'}>
-          My Profile 😁
+      <YStack gap="$lg">
+        <Text color={'$gray9'} fontWeight={'$600'} fontSize={'$h1'}>
+          My{'\n'}Profile 😁
         </Text>
-        <Text> Profile Image</Text>
-        <Link href="/(tabs)/profile/account"> My Account</Link>
-        <Text> Give Feedback</Text>
+        <YStack alignItems="center">
+          <YStack
+            alignItems="center"
+            justifyContent="center"
+            borderRadius={80}
+            borderWidth={6}
+            borderColor={'$purple1'}
+            height={150}
+            width={150}
+          >
+            <YStack
+              borderRadius={60}
+              height={120}
+              width={120}
+              backgroundColor={'$gray6'}
+            ></YStack>
+          </YStack>
+        </YStack>
+
+        <YStack
+          backgroundColor={'$gray4'}
+          paddingVertical="$lg"
+          borderRadius={'$xl'}
+          paddingHorizontal="$md"
+        >
+          <Link href="/(tabs)/profile/account">
+            <XStack gap="$sm" alignItems="center">
+              <Ionicons
+                size={24}
+                name="person"
+                color={getTokens().color.$purple1.val}
+              />
+              <Text fontWeight={'$400'} fontSize={'$md'}>
+                My Account
+              </Text>
+            </XStack>
+          </Link>
+        </YStack>
+
+        <YStack
+          backgroundColor={'$gray4'}
+          paddingVertical="$lg"
+          borderRadius={'$xl'}
+          paddingHorizontal="$md"
+        >
+          <Link href="/(tabs)/profile/account">
+            <XStack gap="$sm" alignItems="center">
+              <Ionicons
+                size={24}
+                name="person"
+                color={getTokens().color.$purple1.val}
+              />
+              <Text fontWeight={'$400'} fontSize={'$md'}>
+                Notifications
+              </Text>
+            </XStack>
+          </Link>
+        </YStack>
+
+        <YStack
+          backgroundColor={'$gray4'}
+          paddingVertical="$lg"
+          borderRadius={'$xl'}
+          paddingHorizontal="$md"
+        >
+          <Link href="/(tabs)/profile/account">
+            <XStack gap="$sm" alignItems="center">
+              <Ionicons
+                size={24}
+                name="person"
+                color={getTokens().color.$purple1.val}
+              />
+              <Text fontWeight={'$400'} fontSize={'$md'}>
+                Give Feedback
+              </Text>
+            </XStack>
+          </Link>
+        </YStack>
       </YStack>
 
       <YStack paddingBottom={insets.bottom}>
-
-    
-      <Button onPress={() => supabase.auth.signOut()}>
-        <Button.Text>Sign Out</Button.Text>
-      </Button>
+        <Button onPress={() => supabase.auth.signOut()}>
+          <Button.Text>Sign Out</Button.Text>
+        </Button>
       </YStack>
     </YStack>
   );
